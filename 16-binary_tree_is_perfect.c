6fd90f95106e0105b542aeb5e495dc23f945df71
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-18 15:56:17 +0000

    calculate the depth then check its equal in each level

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node
+ *
+ * Return: Height of the tree, 0 if NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t left, right;
+
+if (!tree)
+return (0);
+
+left = binary_tree_height(tree->left);
+right = binary_tree_height(tree->right);
+
+return (1 + (left > right ? left : right));
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node
+ *
+ * Return: 1 if perfect, 0 if not or tree is NULL
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+if (!tree)
+return (0);
+
+/* Leaf node */
+if (!tree->left && !tree->right)
+return (1);
+
+/* Node with both children */
+if (tree->left && tree->right)
+{
+/* Check recursively left and right subtrees AND heights must be equal */
+if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+return (binary_tree_is_perfect(tree->left) &&
+        binary_tree_is_perfect(tree->right));
+}
+
+/* Node with one child only */
+return (0);
+}
+
