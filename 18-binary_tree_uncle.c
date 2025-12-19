3ea3f0f494439712f9c9920e59d0516f2a77a7f0
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-19 14:37:05 +0000

    sibling task used as resource to find uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,5 +1,35 @@
 #include "binary_trees.h"
 
+
+/**
+ *binary_tree_sibling - to get the nighbor of the node
+ *@node: to get his nighbor
+ *
+ *Return: null if empty or no parent, nighbor node  if its there
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+
+
+if (!node)
+return (NULL);
+
+if (!node->parent)
+return (NULL);
+
+if ((node->parent)->left == node)
+{
+return ((node->parent)->right);
+}
+else
+{
+return ((node->parent)->left);
+}
+
+return (NULL);
+}
+
 /**
  *binary_tree_uncle - to get the uncle of the node
  *@node: to get his uncle
