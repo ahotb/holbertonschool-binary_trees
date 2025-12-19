300a47e8fc55f03bc857c6e817eb304d98d348b2
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-12-19 14:34:15 +0000

    sibling task used as resource to find uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,20 +9,8 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-if (!node)
+if (!node || !node->parent || !(node->parent)->parent)
 return (NULL);
 
-if (!(node->parent)->parent)
-return (NULL);
-
-if (((node->parent)->parent)->right == node->parent)
-{
-return (((node->parent)->parent)->left);
-}
-else
-{
-return (((node->parent)->parent)->right);
-}
-
-return (NULL);
+return (binary_tree_sibling(node->parent));
 }
